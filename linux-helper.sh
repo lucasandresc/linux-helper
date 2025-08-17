3e1585b705ccf641147fa0b622933d75dfa3ef71
Lucas Andrés Cabrera <lucas.andres.cabrera@gmail.com>
2025-08-17 00:08:18 -0300

    Linux Helper started: Added Welcome sign and 3 list of arrays with differents commands

---
+++ linux-helper.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "╔════════════════════════════════╗"
+echo "║     Welcome to Linux Learner   ║"
+echo "║                                ║"
+echo "║  Interactive Command Helper    ║"
+echo "║     for Linux Beginners        ║"
+echo "╚════════════════════════════════╝"
+echo ""
+
+echo "Please select an option: "
+
+file_commands=("mv" "ls" "cp" "rm" "find" "touch" "cd" "cat" "less" "pwd" "zip" "tar" "head" "tail" "mkdir" "rmdir" "file" "wc")
+system_commands=("clear" "echo" "man" "sudo" "grep" "uname" "whoami" "chmod" "chown" "which" "history" "ps" "ps aux" "kill" "killall" "df" "du" "nano" "vim" "nvim" "date" "uptime" "free" "lscpu""w")
+network_commands=("ping" "wget" "curl" "ssh")
+
+echo ""
